9fad60ee7f730db1cb79518bce536615ecba5c54
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-20 22:31:15 -0500

    created jumbotron to add to save and search page

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,1 +1,16 @@
-export { default } from "./Jumbotron";
+import React from "react";
+
+function Jumbotron() {
+  return(
+    <div className="jumbotron"
+      style={{
+        textAlign: 'center'
+      }}
+    >
+      <h1 className="display-4">(React) Google Books Search</h1>
+      <p className="lead">Search for and Save Books of Interest</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
