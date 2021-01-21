cae89ed1d55a50f5eecf8536bee17844537871ef
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-20 22:35:10 -0500

    created search bar that changes searchTerm useState after the search button is clicked

---
+++ client/src/components/SearchBar/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import API from "../../utils/API";
+import Button from "../Button"
+function SearchBar({ setSearchResults }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = async () => {
+    console.log("You searched for ", searchTerm);
+    await API.getBooks(searchTerm)
+      .then(resp =>{
+        console.log(resp)
+        setSearchResults(resp?.data?.items)
+      })
+  }
+  return (
+    <div>
+      <h3 style={{ textAlign: "center" }}>Search for a Book!</h3>
+      <div className="input-group input-group-lg">
+        <input value={searchTerm} className="form-control" type="text" onChange={(e) => setSearchTerm(e.target.value)} />
+        <Button children="Search" onClick={() => handleSearch()} />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
