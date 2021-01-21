982530ca6a98d94a38b075f33b7752abf7e0b67e
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-21 03:12:02 -0500

    Added use  Effect to run load books function to get books from database and render the books in the db onto the page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import BookList from '../../components/BookList';
+import API from "../../utils/API";
 
 export default function Saved() {
+  const [savedBooks,setSavedBooks] = useState([]);
+
+    useEffect(()=>{
+      loadBooks();
+    },[])
+
+    function loadBooks(){
+      API.getBooks()
+       .then(res=>{
+           console.log("LOAD BOOKS FUNCTION " + res.data)
+           setSavedBooks(res.data)
+       })
+       .catch(err=>console.log(err))
+    }
+
   return (
-    <div>
-   
+    <div className="container">
+      <div className="container card mt-3">
+        <h2>Saved Books </h2>
+        {savedBooks.length===0 ?(<div></div>):(
+          savedBooks.map((book)=>{
+            return(
+              <BookList
+              key={book.id}
+              title={book.title} 
+              authors={book.authors} 
+              desc={book.description} 
+              image={book.image} 
+              link={book.link}
+              />
+            )
+          }))
+        }
+      </div>
     </div>
   )
 }
