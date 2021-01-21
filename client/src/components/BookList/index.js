93f80ba8956d413ffdd83149bd84d3db598bdba4
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-21 03:08:54 -0500

    Added API call that saves the book after save button is clicked

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import API from "../../utils/API";
 
 export default function BookList({title, authors,desc,image,link}) {
+
+  const saveBook = () =>{
+    console.log(`SAVING"${title} book`)
+    API.saveBook({
+      title:title,
+      authors:authors,
+      description:desc,
+      image:image,
+      link:link
+    }).then(result=>{
+        console.log(result)
+    }).catch(err=>console.log(err))
+  }
+
+  //call API delete function and delete book by id then render new saved books list
   return (
     <div className="container card mt-3">
      <div className="row">
@@ -15,6 +31,10 @@ export default function BookList({title, authors,desc,image,link}) {
           View 
         </button>
       </a>
+      <button className="btn btn-primary" onClick={saveBook} style={{float: "right"}}>
+          Save
+      </button>
+      {/* Create delete button and set onChange to delete function */}
           
      </div >
     </div > 
